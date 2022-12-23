7427094f85227cd70b07d1af640c524f4ab5b3b7
unknown <eesuolaayot@gmail.com>
2022-12-23 10:02:44 +0100

    changes made

--- script.js
+++ script.js
@@ -11,6 +11,36 @@ faBars.addEventListener('click', (e)=> {
 faTimes.addEventListener('click', (e)=> {
     myUl.classList.remove('showUl')
 })
+
+
+let myLi = document.querySelectorAll('.myLi')
+myLi.forEach( li => {
+    li.addEventListener('click', ()=> {
+        myUl.classList.remove('showUl')
+    })
+} )
+
+
+// scrolll up button
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 // faTimes.addEventListener('click', (e)=> {
 //     myUl.classList.remove('showUl')
 // })
